6ba5f50abedefe18a76bcff10df1ba79f9e9ce83
DeveloperUchida <kouei28poiou@icloud.com>
2023-09-29 11:59:48 +0900

    DataUpdte 2023/09/29

---
+++ main4.py
@@ -0,0 +1,25 @@
+import PySimpleGUI as sg
+sg.theme("DarkBrown3")
+layout = [
+    [sg.Text("あなたの秘密をお答えしましょう。")],
+    [sg.T("あなたは何歳？"), sg.I("", k="in1")],
+    [sg.T("お母さんは何歳？"), sg.I("", k="in2")],
+    [sg.B("計算開始", k="btn")],
+    [sg.T(k="txt")]
+]
+win = sg.Window("出生の秘密アプリ", layout, font=(None, 10), size=(350, 150))
+def execute():
+    int1 = int(v["in1"])
+    int2 = int(v["in2"])
+    bmi = int2 - int1
+    int3 = 2023 - int2
+    eto = ["子", "丑", "寅", "卯", "辰", "巳", "午", "未", "申", "酉", "戌", "亥"]
+    etonum = (int3 - 4) % 12
+    txt = f"{eto[etonum]}年生まれのお母さんは{bmi}歳の時にあなたを生みました"
+    win["txt"].update(txt)
+while True:
+    e, v = win.read()
+    if e == "btn": execute()
+    if e == None: break
+win.close()
+
